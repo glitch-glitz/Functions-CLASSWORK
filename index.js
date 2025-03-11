d1fcfd9afec36916ecb6c4b167f2c0ed0e0c35bd
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-03-11 10:02:21 +0300

    Did JS Functions

---
+++ index.js
@@ -0,0 +1,48 @@
+function() {
+    //function body comes here
+}
+
+
+//function with the parameter
+function square(a) {
+    
+}
+//function with multiple parameters
+function sum(a, b) {
+    
+}
+
+//Adding body
+function morning() {
+    console.log("Good morning, it's Tuesday!")
+}
+
+
+function morning() {
+  console.log("Good morning, it's Tuesday!");
+}
+console.log(morning())
+
+function square(a) {
+    console.log(a**2)
+}
+console.log(square(2))
+
+
+function sum(a, b) {
+    console.log(a + b)
+}
+console.log(sum(2, 2))
+console.log(sum(356, 4879))
+
+
+function sums(a, b){
+    if (typeof a === "number" && typeof b === "number") {
+        console.log(a + b)
+    }
+}
+console.log(sums(3, 4))
+console.log(sums("3", 4))
+console.log(sums(23, 4))
+
+
